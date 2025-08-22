b87f61f951071c21defa8ef367fa887375180253
Rahul Changra <changrarahul55@gmail.com>
2025-08-22 20:19:10 +0530

    updated frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [jokes, setJokes] = useState([])
+  
+  return (
+    <>
+      <h1>Chai Aur full-stack</h1>
+      <p>JOKES: {jokes.length}</p>
+      {
+        jokes.map((joke,index) => {
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        })}
+    </>
+  )
+}
+
+export default App
